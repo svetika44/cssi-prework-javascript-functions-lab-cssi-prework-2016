dcc300f81f5215c39bb9b64f61895cc1710378ef
dfenjves <fenjves.daniel@gmail.com>
2016-03-14 13:45:51 -0400

    solved

--- tweets.js
+++ tweets.js
@@ -9,19 +9,28 @@
 function countChars(userString) {
   //your code here
   //don't forget to return the number of characters (use return!)
+  return userString.length
 }
 
 //==============================================================================
 // #2 - String Methods: addHashtag
 //==============================================================================
 //Given a string, add " #yolo" to the end of that string
-
+function addHashtag(userString) {
+  //your code here
+  //don't forget to return the number of characters (use return!)
+  return userString + " #yolo"
+}
 //==============================================================================
 
 //==============================================================================
 // #2 - String Methods: excitedTweet
 //==============================================================================
 //Given a string, add " #yolo" to the end of that string
-
+function excitedTweet(userString) {
+  //your code here
+  //don't forget to return the number of characters (use return!)
+  return userString.toUpperCase() + "!!!"
+}
 //==============================================================================
 
